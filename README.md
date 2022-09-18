64d730e08c63d846abc2d0168fe01949550523c5
杨坤海 <384037191@qq.com>
2022-09-18 18:44:29 +0800

    add climbing-stairs

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # LeetCode 解题
+记录 leetcode 解题代码
 
 ## 哈希
 -  [1.两数之和](https://leetcode.cn/problems/two-sum/) [code](./hash/1-%E4%B8%A4%E6%95%B0%E4%B9%8B%E5%92%8C.js)
@@ -13,5 +14,6 @@
 -  [20. 有效的括号](https://leetcode.cn/problems/valid-parentheses/) [code](./stack/20-%E6%9C%89%E6%95%88%E6%8B%AC%E5%8F%B7.js)
 
 ## 动态规划
+- [70. 爬楼梯](https://leetcode.cn/problems/climbing-stairs/) [code](./dynamic/70-%E7%88%AC%E6%A5%BC%E6%A2%AF.js)
 - [300. 最长上升子序列](https://leetcode.cn/problems/longest-increasing-subsequence/) [code](./dynamic/300-%E6%9C%80%E9%95%BF%E4%B8%8A%E5%8D%87%E5%AD%90%E5%BA%8F%E5%88%97.js)
 - [322. 零钱兑换](https://leetcode.cn/problems/coin-change/) [code](./dynamic/322-%E9%9B%B6%E9%92%B1%E5%85%91%E6%8D%A2.js)
---
+++ dynamic/70-爬楼梯.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// 动态规划解法
+// 递推公式 F(n) = F(n-1) + F(n-2)
+var climbStairs = function (n) {
+  let dp = [0, 1, 2]; // 零级台阶，一级台阶，二级台阶
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
+// 递归加记忆化
+// var climbStairs = function (n) {
+//   let memo = new Map();
+//   function recursion(n) {
+//     // n=1 return 1,n=2 return 2
+//     if (n < 3) {
+//       return n;
+//     }
+//     if (memo.has(n)) {
+//       return memo.get(n);
+//     }
+//     return memo.has(n)
+//       ? memo.get(n)
+//       : memo.set(n, recursion(n - 1) + recursion(n - 2)).get(n);
+//   }
+//   return recursion(n);
+// };
+// 递推
+// var climbStairs = function (n) {
+//   let prev = 1; // 一级台阶
+//   let prev2 = 2; // 二级台阶
+//   let total = 0;
+
+//   for (let i = 1; i <= n; i++) {
+//     if (i < 3) {
+//       total = i === 1 ? prev : prev2;
+//     } else {
+//       total = prev2 + prev;
+//       prev = prev2;
+//       prev2 = total;
+//     }
+//   }
+//   return total;
+// };
