6def061ecde6f701cfa303c1fbb095a261eb6ae4
杨坤海 <384037191@qq.com>
2022-09-23 21:34:56 +0800

    add commit

--- two-points/15-三数之和.js
+++ two-points/15-三数之和.js
@@ -7,7 +7,7 @@ var threeSum = function (nums) {
   if (nums.length < 3) {
     return [];
   }
-  // 暴力解法 超时
+  // 先排序，方便后续用遍历
   let sorted = nums.sort((a, b) => a - b);
   let res = [];
   for (let i = 0; i < sorted.length - 2; i++) {
@@ -24,6 +24,7 @@ var threeSum = function (nums) {
         r = r - 1;
       } else {
         res.push([sorted[i], sorted[l], sorted[r]]);
+        // 忽略重复元素
         while (l < r && sorted[l] === sorted[l + 1]) {
           l = l + 1;
         }
